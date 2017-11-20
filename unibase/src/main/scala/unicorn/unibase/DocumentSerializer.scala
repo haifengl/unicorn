7793b1efcd10b6760cf6905ead523e21180b5cd1
Haifeng Li <haifeng.hli@gmail.com>
2017-11-20 15:27:33 -0500

    simplify document serializer

--- unibase/src/main/scala/unicorn/unibase/DocumentSerializer.scala
+++ unibase/src/main/scala/unicorn/unibase/DocumentSerializer.scala
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * (C) Copyright 2015 ADP, LLC.
+ * (C) Copyright 2017 Haifeng Li
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,64 +21,40 @@ import java.nio.ByteBuffer
 import unicorn.bigtable.{Column, ColumnFamily}
 import unicorn.json._
 
-/** Document serializer. By default, document key size is up to 64KB, column size is up to 10MB.
+/** Document serializer.
   *
   * @author Haifeng Li
   */
-class DocumentSerializer(
-  val keySerializer: BsonSerializer = new BsonSerializer(ByteBuffer.allocate(65536)),
-  val valueSerializer: ColumnarJsonSerializer = new ColumnarJsonSerializer(ByteBuffer.allocate(10485760))) {
+class DocumentSerializer {
+  val serializer = new ColumnarJsonSerializer(ByteBuffer.allocate(1048576))
+  val pathDelimiter = serializer.pathDelimiter
 
   /** Serialize document data. */
   def serialize(json: JsObject): Seq[Column] = {
-    valueSerializer.serialize(json).map { case (path, value) =>
-      Column(valueSerializer.str2Bytes(path), value)
+    serializer.serialize(json).map { case (path, value) =>
+      Column(serializer.str2Bytes(path), value)
     }.toSeq
-  }
-
-  /** Serialize document id. */
-  def serialize(tenant: JsValue, id: JsValue): Array[Byte] = {
-    keySerializer.clear
-    keySerializer.put(tenant)
-    keySerializer.put(id)
-    keySerializer.toBytes
-  }
-
-  /** Return the row prefix of a tenant. */
-  def tenantRowKeyPrefix(tenant: JsValue): Array[Byte] = {
-    keySerializer.clear
-    keySerializer.put(tenant)
-    keySerializer.toBytes
-  }
-
-  /** Deserialize document key. */
-  def deserialize(key: Array[Byte]): (JsValue, JsValue) = {
-    val buffer = ByteBuffer.wrap(key)
-    val tenant = keySerializer.deserialize(buffer)
-    val id = keySerializer.deserialize(buffer)
-    (tenant, id)
   }
 
   /** Assembles the document from multi-column family data. */
   def deserialize(data: Seq[ColumnFamily]): Option[JsObject] = {
-    val objects = data.map { case ColumnFamily(family, columns) =>
+    val objects = data.map { case ColumnFamily(_, columns) =>
       val map = columns.map { case Column(qualifier, value, _) =>
-        (new String(qualifier, valueSerializer.charset), value.bytes)
+        (new String(qualifier, serializer.charset), value.bytes)
       }.toMap
-      val json = valueSerializer.deserialize(map)
+      val json = serializer.deserialize(map)
       json.asInstanceOf[JsObject]
     }
 
-    if (objects.size == 0)
-      None
-    else if (objects.size == 1)
-      Some(objects(0))
-    else {
-      val fold = objects.foldLeft(JsObject()) { (doc, family) =>
-        doc.fields ++= family.fields
-        doc
-      }
-      Some(fold)
+    objects.size match {
+      case 0 => None
+      case 1 => Some(objects(0))
+      case _ =>
+        val fold = objects.foldLeft(JsObject()) { (doc, family) =>
+          doc.fields ++= family.fields
+          doc
+        }
+        Some(fold)
     }
   }
 }
