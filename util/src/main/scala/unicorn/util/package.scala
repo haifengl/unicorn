7b38b51750f74b86d2ebafa89a22989eba956ef5
Haifeng Li <haifeng.hli@gmail.com>
2017-11-19 23:03:58 -0500

    clean up util package object

--- util/src/main/scala/unicorn/util/package.scala
+++ util/src/main/scala/unicorn/util/package.scala
@@ -16,7 +16,6 @@
 
 package unicorn
 
-import java.nio.ByteBuffer
 import java.nio.charset.Charset
 import java.time.format.DateTimeFormatter
 
@@ -32,99 +31,11 @@ package object util {
 
   val utf8 = Charset.forName("UTF-8")
 
-  implicit def boxByteArray(x: Array[Byte]) = new ByteArray(x)
-  implicit def unboxByteArray(x: ByteArray) = x.bytes
-  implicit def string2Bytes(x: String) = x.getBytes(utf8)
-  implicit def string2ByteArray(x: String) = new ByteArray(x.getBytes(utf8))
-  implicit def bytesSeq2ByteArray(x: Seq[Array[Byte]]) = x.map { bytes => new ByteArray(bytes) }
-  implicit def stringSeq2ByteArray(x: Seq[String]) = x.map { s => new ByteArray(s.getBytes(utf8)) }
-
   /** Measure running time of a function/block. */
   def time[A](f: => A) = {
     val s = System.nanoTime
     val ret = f
     println("time: " + (System.nanoTime - s)/1e6 + " ms")
     ret
-  }
-
-  /** Helper function convert ByteBuffer to Array[Byte]. */
-  implicit def byteBuffer2ArrayByte(buffer: ByteBuffer): Array[Byte] = {
-    val bytes = new Array[Byte](buffer.position)
-    buffer.position(0)
-    buffer.get(bytes)
-    bytes
-  }
-
-  /** Helper function convert ByteBuffer to ByteArray. */
-  implicit def byteBuffer2ByteArray(buffer: ByteBuffer): ByteArray = ByteArray(byteBuffer2ArrayByte(buffer))
-
-  /** Byte array to hexadecimal string. */
-  def bytes2Hex(bytes: Array[Byte]): String = {
-    bytes.map("%02X" format _).mkString
-  }
-
-  /** Hexadecimal string to byte array. */
-  def hex2Bytes(s: String): Array[Byte] = {
-    require(s.length % 2 == 0, "Hexadecimal string must contain an even number of characters")
-
-    val bytes = new Array[Byte](s.length / 2)
-    for (i <- 0 until s.length by 2) {
-      bytes(i/2) = java.lang.Integer.parseInt(s.substring(i, i+2), 16).toByte
-    }
-    bytes
-  }
-
-  val md5Encoder = java.security.MessageDigest.getInstance("MD5")
-
-  /** MD5 hash function */
-  def md5(bytes: Array[Byte]) = md5Encoder.digest(bytes)
-
-  /** Byte array ordering */
-  def compareByteArray(x: Array[Byte], y: Array[Byte]): Int = {
-    val n = Math.min(x.length, y.length)
-    for (i <- 0 until n) {
-      val a: Int = x(i) & 0xFF
-      val b: Int = y(i) & 0xFF
-      if (a != b) return a - b
-    }
-    x.length - y.length
-  }
-
-  /** Left pad a String with a specified character.
-    *
-    * @param str  the String to pad out, may be null
-    * @param size  the size to pad to
-    * @param padChar  the character to pad with
-    * @return left padded String or original String if no padding is necessary,
-    *         null if null String input
-    */
-  def leftPad(str: String, size: Int, padChar: Char = ' '): String = {
-    if (str == null)
-      return null
-
-    val pads = size - str.length
-    if (pads <= 0)
-      return str // returns original String when possible
-
-    return (String.valueOf(padChar) * pads).concat(str)
-  }
-
-  /** Right pad a String with a specified character.
-    *
-    * @param str  the String to pad out, may be null
-    * @param size  the size to pad to
-    * @param padChar  the character to pad with
-    * @return left padded String or original String if no padding is necessary,
-    *         null if null String input
-    */
-  def rightPad(str: String, size: Int, padChar: Char = ' '): String = {
-    if (str == null)
-      return null
-
-    val pads = size - str.length
-    if (pads <= 0)
-      return str // returns original String when possible
-
-    return str.concat(String.valueOf(padChar) * pads)
   }
 }
